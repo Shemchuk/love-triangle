4f36aa14c96d659e44c6c3d5c21f34dbf7d047f4
Mikhail Shemchuk <mihey.by@gmail.com>
2019-03-20 21:22:17 +0300

    feat: create body of function getLoveTrianglesCount()

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let skp = [];
+  let res = 0;
+
+  for (i in preferences) {
+    if (skp.indexOf(i * 1) > -1) {
+      continue;
+    }
+    let first = [preferences[i], i * 1];
+    let last = first;
+    let s = [];
+
+    for (j = 1; j < 4; j++) {
+
+      if (last.join() == [preferences[last[0] - 1], last[0] - 1].join()) {
+        last = [];
+        break;
+      }
+      last = [preferences[last[0] - 1], last[0] - 1];
+      s.push(last[0] - 1);
+    }
+
+    if (first.join() == last.join()) {
+      res++;
+      skp = skp.concat(s)
+    }
+  }
+  return res;
 };
